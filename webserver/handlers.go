3824b199ef55b2275c38ac9b43b97c0272e2f110
kz <zaramaluna1999@hotmail.com>
2020-07-04 19:19:31 -0500

    handler user and path create added

--- webserver/handlers.go
+++ webserver/handlers.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"encoding/json"
 )
 
 func HandleRoot(w http.ResponseWriter, r *http.Request){
@@ -11,4 +12,15 @@ func HandleRoot(w http.ResponseWriter, r *http.Request){
 
 func HandleHome(w http.ResponseWriter, r *http.Request){
 	fmt.Fprintf(w, "This is the API endpoint")
+}
+
+func PostRequest(w http.ResponseWriter, r *http.Request){
+	decoder := json.NewDecoder(r.Body)
+	var metadata MetaData
+	err := decoder.Decode(&metadata)
+	if err != nil {
+		fmt.Fprintf(w, "error: %v", err)
+		return
+	}
+	fmt.Fprintf(w, "Payload %v\n", metadata)
 }
--- webserver/main.go
+++ webserver/main.go
@@ -3,6 +3,7 @@ package main
 func main() {
 	server := new_server(":3000")
 	server.Handle("GET", "/", HandleRoot)
+	server.Handle("POST", "/create", PostRequest)
 	server.Handle("POST", "/api", server.AddMiddleware(HandleHome, CheckAuth(), Logging()))
 	server.Listen()
 }
