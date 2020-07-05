f4125f1d45d2c9dce331c9de81a4b3dc15931ef2
kz <zaramaluna1999@hotmail.com>
2020-07-04 19:05:46 -0500

    methods added in the paths

--- webserver/router.go
+++ webserver/router.go
@@ -5,26 +5,32 @@ import (
 )
 
 type Router struct {
-	rules map[string]http.HandlerFunc
+	rules map[string]map[string]http.HandlerFunc
 }
 
 func new_router() *Router {
 	return &Router{
-		rules : make(map[string]http.HandlerFunc),
+		rules : make(map[string]map[string]http.HandlerFunc),
 	}
 }
 
-func (r *Router) findHandler(path string) (http.HandlerFunc, bool) {
-	handler, exist := r.rules[path]
-	return handler, exist
+func (r *Router) findHandler(path string, method string) (http.HandlerFunc, bool, bool) {
+	_, exist := r.rules[path]
+	handler, methodExists := r.rules[path][method]
+	return handler, methodExists, exist
 }
 
 func (r *Router) ServeHTTP(w http.ResponseWriter, request *http.Request) {
-	handler, exist := r.findHandler(request.URL.Path)
+	handler, methodExists, exist := r.findHandler(request.URL.Path, request.Method)
 
 	if !exist {
 		w.WriteHeader(http.StatusNotFound)
 		return 
+	}
+
+	if !methodExists {
+		w.WriteHeader(http.StatusMethodNotAllowed)
+		return
 	}
 
 	handler(w, request)
